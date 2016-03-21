0c01a6b9b17d22235c7ca8f91905120879f9a561
Paul Chery <paulachery@gmail.com>
2016-03-21 00:37:02 -0500

    Added attributes to controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306012233) do
+ActiveRecord::Schema.define(version: 20160321052444) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,11 +21,13 @@ ActiveRecord::Schema.define(version: 20160306012233) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "videos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
